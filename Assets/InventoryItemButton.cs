e4fae25ed68f5f4aaa184df092393191218b18d8
Curban841 <curban841@gmail.com>
2020-11-08 16:16:44 -0800

    tutorial 4 completed

---
+++ Assets/InventoryItemButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InventoryItemButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Text buttonText;
+    private string[] itemTypes = { "Armor", "Weapon", "Spell" };
+    public int typeIndex;
+    void Awake()
+    {
+        typeIndex = Random.Range(0, 3);
+        buttonText = GetComponentInChildren<Text>();
+        buttonText.text = itemTypes[typeIndex];
+    }
+}
---
+++ Assets/InventroyDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventroyDisplay : MonoBehaviour
+{
+    public void ShowOnly(int itemType)
+	{
+		for (int i = 0; i < transform.childCount; i++)
+		{
+   			InventoryItemButton thisItem = transform.GetChild(i).GetComponent<InventoryItemButton>();
+    			thisItem.gameObject.SetActive(thisItem.typeIndex == itemType);
+		}
+	}
+
+	public void ShowAll()
+	{
+		for (int i = 0; i < transform.childCount; i++)
+		{
+    		transform.GetChild(i).gameObject.SetActive(true);
+		}
+	}
+}
